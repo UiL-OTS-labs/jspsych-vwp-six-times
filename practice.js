a7d2fa612172eb983584d4350f6ede6ac6bf19be
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2026-05-13 10:11:29 +0200

    Patch practice.js to match missing stimulus

--- practice.js
+++ practice.js
@@ -1,14 +1,13 @@
 "use-strict"
-
 // The stimuli for the practice phase
 const PRACTICE_ITEMS = [
     {
         id: 1,
         item_type: PRACTICE,
         image: './img/13e.png',
-        sound: './sounds/mkd_L_der_orca_liebt_johann.wav',
+        sound: './sounds/mkd_R_der_orca_liebt_johann.wav',
         bias_onset: 1234, // After 1234 ms the sentence should cause a bias in looking behavior
         expected_answer: true,
-        target_pos: LEFT
+        target_pos: RIGHT
     },
 ];
